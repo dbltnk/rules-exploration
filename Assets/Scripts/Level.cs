ed0e7c35f4e89d9f755112d23462a947df244dc4
Alexander Zacherl <alexander.zacherl@googlemail.com>
2021-04-09 22:33:30 +0100

    level fix

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -32,14 +32,14 @@ public class Level : ScriptableObject
         EditorUtility.SetDirty(bankScript.gameObject);
     }
 
-    [ExecuteInEditMode]
-    public void OnDestroy () {
-        List<Level> gatheredLevels = new List<Level>();
-        gatheredLevels.AddRange(bankScript.GetLevels());
-        for (int i = 0; i < gatheredLevels.Count; i++) {
-            if (gatheredLevels[i] == this) gatheredLevels.Remove(gatheredLevels[i]);
-        }
-        bankScript.AssignLevels(gatheredLevels.ToArray());
-        EditorUtility.SetDirty(bankScript.gameObject);
-    }
+    //Disabled because Unity never calls this. Likely a bug in the engine.
+    //public void OnDestroy () {
+    //    List<Level> gatheredLevels = new List<Level>();
+    //    gatheredLevels.AddRange(bankScript.GetLevels());
+    //    for (int i = 0; i < gatheredLevels.Count; i++) {
+    //        if (gatheredLevels[i] == this) gatheredLevels.Remove(gatheredLevels[i]);
+    //    }
+    //    bankScript.AssignLevels(gatheredLevels.ToArray());
+    //    EditorUtility.SetDirty(bankScript.gameObject);
+    //}
 }
