d0f25710890064c658395ad7dc7bac7df3fe1b47
Alexander Zacherl <alexander.zacherl@googlemail.com>
2021-04-09 22:33:12 +0100

    level banks now get auto-filled and (kinda) cleaned up as well

--- Assets/Scripts/LevelBankScript.cs
+++ Assets/Scripts/LevelBankScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class LevelBankScript : MonoBehaviour
 {
@@ -9,20 +10,28 @@ public class LevelBankScript : MonoBehaviour
     public void AssignLevels(Level[] levelArray)
     {
         allLevels = levelArray;
+        RemoveNullReferences();
     }
 
     public Level[] GetLevels()
     {
+        RemoveNullReferences();
         return allLevels;
     }
 
     public Level GetLevelByIndex(int index)
     {
+        RemoveNullReferences();
         return allLevels[index];
     }
 
     public Level GetRandomLevel()
     {
+        RemoveNullReferences();
         return allLevels[Random.Range(0, allLevels.Length)];
+    }
+
+    public void RemoveNullReferences() {
+        allLevels = allLevels.Where(t => t != null).ToArray();
     }
 }
