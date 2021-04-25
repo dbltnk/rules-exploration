ece975a390a8252bc5e2f6fd2a285ceb19d17599
Alexander Zacherl <alexander.zacherl@googlemail.com>
2021-04-25 21:05:50 +0100

    diagram now updates for all species and zero population

--- Assets/Scripts/LayerStatusScript.cs
+++ Assets/Scripts/LayerStatusScript.cs
@@ -65,60 +65,14 @@ public class LayerStatusScript : MonoBehaviour
     {
         populationBySpecies = populationDictionary;
 
-        CalcAndDisplayTopThree(populationReport);
+        CalcAndDisplayAll(populationReport);
     }
 
-    void CalcAndDisplayTopThree(List<PopulationCount> populationCount)
+    void CalcAndDisplayAll(List<PopulationCount> populationCount)
     {
-        PopulationCount[] newPopulationRanking = new PopulationCount[]
-        {
-            new PopulationCount(null, 0),
-            new PopulationCount(null, 0),
-            new PopulationCount(null, 0),
-        };
-
-        for(int i = 0; i < populationCount.Count; i++)
-        {
-            PopulationCount newEntry = populationCount[i];
-
-            int newValue = newEntry.population;
-
-            if(newValue > newPopulationRanking[0].population)
-            {
-                newPopulationRanking[2] = newPopulationRanking[1];
-                newPopulationRanking[1] = newPopulationRanking[0];
-                newPopulationRanking[0] = newEntry;
-            }
-            else if(newValue > newPopulationRanking[1].population)
-            {
-                newPopulationRanking[2] = newPopulationRanking[1];
-                newPopulationRanking[1] = newEntry;
-            }
-            else if(newValue > newPopulationRanking[2].population)
-            {
-                newPopulationRanking[2] = newEntry;
-            }            
-        }
-
-        for (int i = 0; i < newPopulationRanking.Length; i++)
-        {
-            PopulationCount currentPopulationCount = newPopulationRanking[i];
-
-            if(currentPopulationCount.species == null)
-            {
-                topThreeReadout[i].text = "---NULL---";
-                // TODO Add zeros when we do not have any members of a species
-                // foreach (GameObject line in lines) {
-                //     if (line.GetComponent<DD_Lines>().lineName == currentPopulationCount.species.defaultName) diagram.InputPoint(line, new Vector2(1, 0));
-                // }
-            }
-            else
-            {
-                // TODO Make this update for all species in the level, not just the top three.
-                topThreeReadout[i].text = string.Format("{0}: {1}", speciesBank.GetSpeciesName(currentPopulationCount.species), currentPopulationCount.population);
-                foreach (GameObject line in lines) {
-                    if (line.GetComponent<DD_Lines>().lineName == currentPopulationCount.species.defaultName) diagram.InputPoint(line, new Vector2(1, currentPopulationCount.population));
-                }
+        foreach (PopulationCount popCount in populationCount) {
+            foreach (GameObject line in lines) {
+                if (line.GetComponent<DD_Lines>().lineName == popCount.species.defaultName) diagram.InputPoint(line, new Vector2(1, popCount.population));
             }
         }
     }
